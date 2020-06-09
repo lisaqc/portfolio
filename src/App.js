6c0ab734c767550542fe3f5cb4de70ee5579e8a4
Lisa Chen <lisachen@Lisas-MacBook-Air-2.local>
2020-06-09 18:07:27 -0400

    adding 2020

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-         Coming Soon ...
+         Coming Soon ... 2020
         </p>
       </header>
     </div>
