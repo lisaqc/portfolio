46121f251db735d2bf1dfd51d6fd87bcfd514cdc
mrchenliang <c.liang@ualberta.ca>
2020-07-02 09:24:47 -0400

    Updates

---
+++ precache-manifest.3c8d2d3daff20898e8efa34688bcddef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f0175eb011701ec82ed4345c9d6638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de2a055d1ede11d4da4b",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "71f31cb1d44cf9aa19fa",
+    "url": "/static/css/main.16e124bf.chunk.css"
+  },
+  {
+    "revision": "de2a055d1ede11d4da4b",
+    "url": "/static/js/2.afe72e38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afe72e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f31cb1d44cf9aa19fa",
+    "url": "/static/js/main.4dd26b67.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "395da6c48bbfe67ff850c5cf0962012b",
+    "url": "/static/media/image.395da6c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2f5ee7d01b2d4ccbb775aefe84dd616.js"
+  "/precache-manifest.3c8d2d3daff20898e8efa34688bcddef.js"
 );
 
 self.addEventListener('message', (event) => {
