b5bfeb57b523f53e904a59ddb6cd7a0a85028f0b
mrchenliang <c.liang@ualberta.ca>
2020-07-05 21:18:17 -0500

    reversing changes

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <ul class="menu__list">
                 <li class="menu__list-item"><a class="menu__link" href="/home">Home</a></li>
                 <li class="menu__list-item"><a class="menu__link" href="/about">About</a></li>
-                <li class="menu__list-item"><a class="menu__link" href="/resume" target="_blank">Resume</a></li>
+                <li class="menu__list-item"><a class="menu__link" href="/resume">Resume</a></li>
             </ul>
         </navbar>
     );
