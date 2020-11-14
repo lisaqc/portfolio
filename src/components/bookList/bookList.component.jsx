ec7b0caca1456ba91dddb0e839759e6ce0438a21
Lisa Chen <lisachen@Lisas-MacBook-Pro.local>
2020-11-13 23:19:10 -0700

    added new books

--- src/components/bookList/bookList.component.jsx
+++ src/components/bookList/bookList.component.jsx
@@ -3,11 +3,14 @@ import './bookList.styles.scss';
 
 const BookList = () => {
     const books = [
+        {title: "Shoe Dog", author: "Phil Knight"},
+        {title: "American Dirt", author: "Jeanine Cummins"},
         {title: "Becoming", author: "Michelle Obama"},
         {title: "Recursion", author: "Blake Crouch"},
         {title: "The Help", author: "Kathryn Stockett"},
         {title: "Bad Blood", author: "John Carreyrou"},
         {title: "Normal People", author: "Sally Rooney"},
+        {title: "Convenience Store Woman", author: "Sayaka Murata"},
         {title: "The Uninhabitale Earth", author: "David Wallace-Wells"},
         {title: "Where the Crawdads Sing", author: "Delia Owens"},
         {title: "Design Is How It Works", author: "Jay Greene"},
