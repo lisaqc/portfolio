5cfc7567f305f10379cf21292565094009c97eb9
Chen Liang <chenliang@Chens-MacBook-Pro.local>
2020-11-23 09:35:06 -0700

    fixing top margin for medium, and fixing grammar error

--- src/components/aboutMe/aboutMe.component.jsx
+++ src/components/aboutMe/aboutMe.component.jsx
@@ -11,7 +11,7 @@ const AboutMe = (props) => {
             </div>
             <div className = 'about-me-description'>
                 <div className = 'about-me-body'>
-                    Currently, I worked at <b>American Express</b> as a <b>Product Marketing Manager</b> focused on the Early Engagement portion ofthe customer journey. My goal is to make the onboarding process as smooth as possible for a new cardmember in all the platforms they may use. This includes the multiple digital activation journeys, as well as the onboarding marketing curriculum.
+                    Currently, I work at <b>American Express</b> as a <b>Product Marketing Manager</b> focused on the Early Engagement portion ofthe customer journey. My goal is to make the onboarding process as smooth as possible for a new cardmember in all the platforms they may use. This includes the multiple digital activation journeys, as well as the onboarding marketing curriculum.
                 </div>
             </div>
         </div>
